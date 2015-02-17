79f25e6647f92b73b8fd1c4f4a9403bba9327bb5
Saman Jahanbakhsh <samanj@kth.se>
2015-02-17 12:34:35 +0100

    my first commit

---
+++ src/main/java/com/mycompany/web/services/hw3/authenticate/Person.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.web.services.hw3.authenticate;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author syst3m
+ */
+@Entity
+@Table(name = "person")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Person.findAll", query = "SELECT p FROM Person p"),
+    @NamedQuery(name = "Person.findByUsername", query = "SELECT p FROM Person p WHERE p.username = :username"),
+    @NamedQuery(name = "Person.findByPasswrod", query = "SELECT p FROM Person p WHERE p.passwrod = :passwrod"),
+    @NamedQuery(name = "Person.findByLoogin", query = "SELECT p FROM Person p WHERE p.loogin = :loogin")})
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "username")
+    private String username;
+    @Size(max = 45)
+    @Column(name = "passwrod")
+    private String passwrod;
+    @Size(max = 1)
+    @Column(name = "loogin")
+    private String loogin;
+
+    public Person() {
+    }
+
+    public Person(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswrod() {
+        return passwrod;
+    }
+
+    public void setPasswrod(String passwrod) {
+        this.passwrod = passwrod;
+    }
+
+    public String getLoogin() {
+        return loogin;
+    }
+
+    public void setLoogin(String loogin) {
+        this.loogin = loogin;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (username != null ? username.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Person)) {
+            return false;
+        }
+        Person other = (Person) object;
+        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.web.services.hw3.authenticate.Person[ username=" + username + " ]";
+    }
+    
+}
